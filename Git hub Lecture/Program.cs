e1911619d8e809c641efdd5ba6d73491e2b0844a
Raston Gilbert <rastong@gmail.com>
2021-10-07 13:22:03 -0400

    first commit

---
+++ Git hub Lecture/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Git_hub_Lecture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        public static bool Larger1(int a, int b)
+        {
+            
+        }
+
+        public static bool Larger2(int a, int b)
+        {
+
+        }
+    }
+}
